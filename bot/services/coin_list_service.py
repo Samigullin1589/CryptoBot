4442a07fda4e2ad6e43dff08666de887f8fe7473
Samigullin1589 <mars.samigullin2010@gmail.com>
2025-06-21 14:23:54 +0400

    Fix: Перевел coin_list_service на async-lru

--- bot/services/coin_list_service.py
+++ bot/services/coin_list_service.py
@@ -1,9 +1,8 @@
-# bot/services/coin_list_service.py
 import logging
 from typing import Dict
 
 import aiohttp
-from cachetools import cached, LRUCache
+from async_lru import alru_cache
 
 from bot.config.settings import settings
 from bot.utils.helpers import make_request
@@ -11,27 +10,19 @@ from bot.utils.helpers import make_request
 logger = logging.getLogger(__name__)
 
 class CoinListService:
-    # Кэш, как атрибут класса
-    cache = LRUCache(maxsize=1)
-
-    @cached(cache)
+    @alru_cache(maxsize=1, ttl=21600)  # Используем новый декоратор async-lru с кэшем на 6 часов
     async def get_coin_list(self) -> Dict[str, str]:
-        """
-        Загружает и возвращает словарь, где ключ - тикер монеты, 
-        а значение - ее алгоритм.
-        """
-        logger.info("Updating coin list cache from Minerstat...")
-        coin_algo_map = {}
+        """Получает и кэширует список монет и их алгоритмов с MinerStat."""
+        logger.info("Updating coin list with algorithms from MinerStat...")
         async with aiohttp.ClientSession() as session:
+            # Используем URL из настроек
             data = await make_request(session, f"{settings.minerstat_api_base}/coins")
-            if data:
-                for coin_data in data:
-                    if symbol := coin_data.get('coin'):
-                        coin_algo_map[symbol.upper()] = coin_data.get('algorithm')
-        
-        if coin_algo_map:
-            logger.info(f"Coin list cache updated with {len(coin_algo_map)} coins.")
-        else:
-            logger.warning("Failed to update coin list cache. Data was empty.")
-
-        return coin_algo_map
+            if data and isinstance(data, list):
+                logger.info(f"Successfully fetched {len(data)} coins from MinerStat.")
+                # Создаем словарь {ТИКЕР: АЛГОРИТМ}
+                return {
+                    coin['coin']: coin['algorithm']
+                    for coin in data if 'coin' in coin and 'algorithm' in coin
+                }
+        logger.warning("Failed to fetch coin list from MinerStat. Returning empty dict.")
+        return {}
